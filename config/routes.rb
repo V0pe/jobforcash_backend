77ee446384072914e4ba2c6fd27e6e98a07ccc57
V0pe <dampblew01@gmail.com>
2023-06-08 14:36:12 -0600

    Add the application routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+   namespace :v1, defaults: { format: 'json' } do
+    resources :users, only: [:create ]
+    resources :laborers, only: [:index, :show, :create, :destroy]
+    resources :reservations, only: [:index, :show, :create, :destroy]
+  end
 end
