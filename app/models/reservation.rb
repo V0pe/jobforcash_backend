06fcc0293b825a0e334332ebfb432f46c11170c1
German Renato Cobian <germancobian@hotmail.com>
2023-06-09 13:49:28 -0700

    Add references and validatyions to Reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,9 @@
 class Reservation < ApplicationRecord
+
+  belongs_to :user
+  belongs_to :laborer
+
+  validates :start_date, presence: true
+  validates :number-days, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 30 }
+
 end
