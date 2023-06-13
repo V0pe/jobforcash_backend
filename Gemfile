d2f3dba8ac9a8f3cbaef3d799e507fca8a3bfd67
German Renato Cobian <germancobian@hotmail.com>
2023-06-13 12:18:12 -0700

    Add authentication gems + configure devise + create devise User model

--- Gemfile
+++ Gemfile
@@ -42,6 +42,11 @@ gem 'jsonapi-serializer', '~> 2.2'
 # rubocop
 gem 'rubocop', '>= 1.0', '< 2.0'
 
+# Authorization gems
+gem 'devise'
+gem 'devise-jwt'
+gem 'fast_jsonapi'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
    namespace :v1, defaults: { format: 'json' } do
-    resources :users, only: [:show, :create, :destroy ]
     resources :laborers, only: [:index, :show, :create, :destroy]
     resources :reservations, only: [:index, :show, :create, :destroy]
   end
