34ac57d01c1c1a107cd51aaf52d712ae74b89134
German Renato Cobian <germancobian@hotmail.com>
2023-06-13 12:02:48 -0700

    Reconfigure User model to include password

--- db/schema.rb
+++ db/schema.rb
@@ -67,6 +67,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_12_154933) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
