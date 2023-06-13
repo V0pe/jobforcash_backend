36007eb013546093af5d42c8e56a660ff5f1d39f
V0pe <dampblew01@gmail.com>
2023-06-13 12:58:06 -0600

    Create devise controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
 
-  # Defines the root path route ("/")
-  # root "articles#index"
    namespace :v1, defaults: { format: 'json' } do
     resources :laborers, only: [:index, :show, :create, :destroy]
     resources :reservations, only: [:index, :show, :create, :destroy]
